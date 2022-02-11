3aabe4730d4d19f4bf959baad58dd7af196644e0
Dharmull <97678468+Dharmull@users.noreply.github.com>
2022-02-11 10:04:10 +0700

    Create pertemuan 02

---
+++ pertemuan 02/tes2/src/tes2/Tes2.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package tes2;
+
+/**
+ *
+ * @author user
+ */
+public class Tes2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+    System.out.println("   /\\          /\\     ");
+    System.out.println("  /  \\________/  \\    ");
+    System.out.println(" /                 \\   ");
+    System.out.println("(   /\\         /\\  )  ");
+    System.out.println(" =====     v    =====");
+    System.out.println("========(__|__)=======");
+    System.out.println("    (             )");
+    System.out.println("     (___________)");
+    }
+    
+}
